545b62c5cee6b49dc9f63e713a2e3b7b5a2815d7
yagelpinhas <yagel250@gmail.com>
2022-11-24 13:17:23 +0200

    deleted counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import { Inventory } from './features/Inventory/Inventory';
 import './App.css';
 
--- my-app/src/app/store.js
+++ my-app/src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import inventoryReducer from '../features/Inventory/inventorySlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     inventory: inventoryReducer,
   },
 });
